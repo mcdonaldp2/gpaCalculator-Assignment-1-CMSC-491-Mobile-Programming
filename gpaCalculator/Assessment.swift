b633be52b473db340bdc93b4447e1a65d549db23
mcdonaldp2 <mcdonaldp2@vcu.edu>
2016-02-12 17:29:54 -0500

    1st commit,  pretty much done.  will change a few things for error messages and such

---
+++ gpaCalculator/Assessment.swift
@@ -0,0 +1,9 @@
+//
+//  Assessment.swift
+//  gpaCalculator
+//
+//  Created by Paul McDonald  on 2/10/16.
+//  Copyright © 2016 Paul McDonald . All rights reserved.
+//
+
+import Foundation
---
+++ gpaCalculator/Course.swift
@@ -0,0 +1,9 @@
+//
+//  Course.swift
+//  gpaCalculator
+//
+//  Created by Paul McDonald  on 2/8/16.
+//  Copyright © 2016 Paul McDonald . All rights reserved.
+//
+
+import Foundation
